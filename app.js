0eceb3efa0ef6fc45e9bf4a9571c36aebfaa0623
Mateus <mateucmc@gmail.com>
2024-09-15 16:24:03 -0300

    rotas html

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 const denunciaRoutes = require('./routes/denunciaRoutes');
+const staticRoutes = require('./routes/staticRoutes');
 
 const app = express();
 app.use(express.json());
+
+// Servindo CSS e Imagens a partir da pasta public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Servindo o HTML a partir da pasta views
 app.use(express.static(path.join(__dirname, 'views')));
@@ -12,6 +16,7 @@ app.use(express.static(path.join(__dirname, 'views')));
 // Usando as rotas
 app.use('/', userRoutes);
 app.use('/', denunciaRoutes);
+app.use('/', staticRoutes);
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
---
+++ routes/staticRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/TelaInicial.html')
+})
+
+router.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/views/TelaLogin.html')
+})
+
+router.get('/:page', (req, res) => {
+  const page = req.params.page;
+  const filePath = path.join(__dirname, 'views', `${page}.html`);
+
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send('Page not found');
+    }
+  });
+});
+
+module.exports = router;
